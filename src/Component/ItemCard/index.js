21fa7d561a2e551441670f38f4fa5ea9fecb4f69
Sameer Watts <sameerwatts00@gmail.com>
2020-09-03 03:20:34 +0530

    style(): Add content item card

--- src/Component/ItemCard/index.js
+++ src/Component/ItemCard/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ContentWrapper from '../ContentWrapper';
+import "./style.css";
 
-const ItemCard = (props) => {
+const ItemCard = ({ details }) => {
     return (
-        <ContentWrapper className="mt-10">
-            <img src={props.details.links.mission_patch} alt="img"/>
+        <ContentWrapper className="itemCard fw-500 mt-10">
+            <div className="imageContainer">
+                <img src={details.links.mission_patch} alt="missionPic" />
+            </div>
+            <p className="rocketName">{details.mission_name}</p>
+            <p className="mt-10 fw-300"><span className="fw-500">Mission Ids:</span>{details.mission_id.join()}</p>
+            <p className="mt-10 fw-300"><span className="fw-500">Launch Year:</span>{details.launch_year}</p>
+            <p className="mt-10 fw-300"><span className="fw-500">Successful Launch :</span>{details.launch_success.toString()}</p>
+            <p className="mt-10 fw-300"><span className="fw-500">Successful Landing:</span>{details.rocket.first_stage.cores[0].land_success}</p>
         </ContentWrapper>
     );
 };
